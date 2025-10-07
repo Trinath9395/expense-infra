c53ee1b8c76ddc394f276858072a1c96bf998fae
Trinath9395 <trinathdevops0606@gmail.com>
2025-10-07 16:56:52 +0530

    jenkins-terraform-infra

--- 10-sg/sg.tf
+++ 10-sg/sg.tf
@@ -7,9 +7,6 @@
 # -----------------------------
 # Data Sources
 # -----------------------------
-data "aws_ssm_parameter" "vpc_id" {
-  name = "/${var.project_name}/${var.environment}/vpc_id"
-}
 
 # -----------------------------
 # Security Group Modules
