edfbc214525c53e5cbe1819d7780b0b752df0b3c
Trinath9395 <trinathdevops0606@gmail.com>
2025-10-07 16:27:18 +0530

    jenkins-terraform-infra

--- 10-sg/parameter.tf
+++ 10-sg/parameter.tf
@@ -1,41 +1,30 @@
 resource "aws_ssm_parameter" "mysql_sg_id" {
-  type = "String"
-  name = "/${var.project_name}/${var.environment}/mysql_sg_id"
+  name  = "/${var.project_name}/${var.environment}/mysql_sg_id"
+  type  = "String"
   value = module.mysql_sg.sg_id
 }
 
-resource "aws_ssm_parameter" "backend_sg_id" {
-  type = "String"
-  name = "/${var.project_name}/${var.environment}/backend_sg_id"
-  value = module.backend_sg.sg_id
+
+resource "aws_ssm_parameter" "bastion_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/bastion_sg_id"
+  type  = "String"
+  value = module.bastion_sg.sg_id
 }
 
-resource "aws_ssm_parameter" "frontend_sg_id" {
-  type = "String"
-  name = "/${var.project_name}/${var.environment}/frontend_sg_id"
-  value = module.frontend_sg.sg_id 
+resource "aws_ssm_parameter" "alb_ingress_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/alb_ingress_sg_id"
+  type  = "String"
+  value = module.alb_ingress_sg.sg_id
 }
 
-resource "aws_ssm_parameter" "bastion_sg" {
-  type = "String"
-  name = "/${var.project_name}/${var.environment}/bastion_sg_id"
-  value = module.bastion_sg.sg_id 
+resource "aws_ssm_parameter" "eks_control_plane_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/eks_control_plane_sg_id"
+  type  = "String"
+  value = module.eks_control_plane_sg.sg_id
 }
 
-resource "aws_ssm_parameter" "app_alb_sg_id" {
-  type = "String"
-  name = "/${var.project_name}/${var.environment}/app_alb_sg_id"
-  value = module.app_alb_sg.sg_id
-}
-
-resource "aws_ssm_parameter" "vpn_sg_id" {
-  type = "String"
-  name = "/${var.project_name}/${var.environment}/vpn_sg_id"
-  value = module.vpn_sg.sg_id
-}
-
-resource "aws_ssm_parameter" "web_alb_sg" {
-  type = "String"
-  name = "/${var.project_name}/${var.environment}/web_alb_sg"
-  value = module.web_alb_sg.sg_id 
+resource "aws_ssm_parameter" "eks_node_sg_id" {
+  name  = "/${var.project_name}/${var.environment}/eks_node_sg_id"
+  type  = "String"
+  value = module.eks_node_sg.sg_id
 }
