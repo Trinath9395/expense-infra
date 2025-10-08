606a3a45b65a2cd1878f6425a5b71ae15db07dd3
Trinath9395 <trinathdevops0606@gmail.com>
2025-10-08 15:48:17 +0530

    changes are made

--- 20-bastion/bastion.tf
+++ 20-bastion/bastion.tf
@@ -3,6 +3,7 @@
     vpc_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id.value]
     subnet_id = local.public_subnet_id
     instance_type = "t2.micro" 
+    associate_public_ip_address = true
 
    tags = merge(
     var.common_tags,
