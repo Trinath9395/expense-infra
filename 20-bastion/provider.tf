53429676ec9ffe8130cc523f4300b6271deb48af
Trinath9395 <trinathdevops0606@gmail.com>
2025-10-09 09:26:37 +0530

    provider changes are made

--- 20-bastion/provider.tf
+++ 20-bastion/provider.tf
@@ -7,7 +7,7 @@ terraform {
   }
 
   backend s3 {
-  bucket = "82s-tf-remote-state-dev-pr"
+  bucket = "82-remote-state-pr-prod"
   key = "sg-infra-eks-bastion"
   region = "us-east-1"
   use_lockfile = false
